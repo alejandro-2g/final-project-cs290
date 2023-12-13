0083a065bde97dc76eec427b1a028bd35e475c3e
Miguel Saavedra <miguels78.ms@gmail.com>
2023-12-12 18:17:53 -0800

    .json files and server side. Started implenting middleware functions but not sure how they'll work yet since I haven't tested them. Changed fields in Data.json to more easily code w/ in server.js. Added MyPlanData.json to hold the data for users plan.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 var express = require('express')
 var data = require('./data.json')
+var MyPlanData = require('./MyPlanData.json')
 var exphbs = require('express-handlebars')
 
 var app = express()
@@ -11,6 +12,7 @@ app.set("view engine", "handlebars")
 
 var port = process.env.PORT || 3000
 
+app.use(express.json())
 app.use(express.static('public'))
 
 app.get('/', function(req, res, next){
@@ -34,6 +36,40 @@ app.get('/workouts/:muscleGroup', function(req, res, next){
         next()
     }
 })
+
+// app.post('/workouts/:muscleGroup/addMyPlan', function (req, res, next) {
+//     var muscleGroup = req.params.muscleGroup.toLowerCase()
+//     var exerciseData = data[muscleGroup]
+//     if (exerciseData) {
+//       if (req.body && req.body.url && req.body.exerciseName) {
+//         MyPlanData.exercises.push({
+//           exerciseName: req.body.exerciseName,
+//           url: req.body.url
+//         })
+  
+//         fs.writeFile(
+//           "./MyPlanData.json",
+//           JSON.stringify(MyPlanData, null, 2),
+//           function (err) {
+//             if (err) {
+//               res.status(500)
+//             } else {
+//               res.status(200)
+//             }
+//           }
+//         )
+//       } else {
+//         res.status(400)
+//       }
+//     } else {
+//       next()
+//     }
+//   })
+
+// app.delete('/workouts/:muscleGroup/removeMyPlan', function(req, res, next){
+//     var muscleGroup = req.params.muscleGroup.toLowerCase()
+//     var exerciseData = MyPlanData[]
+// })
 
 app.get('*', function (req, res) {
     res.status(404).render("404Page", {url: req.originalUrl})
